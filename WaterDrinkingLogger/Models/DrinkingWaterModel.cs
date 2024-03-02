ead6d7c81299a89b1404a5d7f55d94962ad73d30
ionutng <ionutngp@gmail.com>
2024-03-02 11:55:55 +0200

    Set up connection to the database and add a create form

---
+++ WaterDrinkingLogger/Models/DrinkingWaterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WaterDrinkingLogger.Models;
+
+public class DrinkingWaterModel
+{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ WaterDrinkingLogger/Pages/Create.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using WaterDrinkingLogger.Models;
+
+namespace WaterDrinkingLogger.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public CreateModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DrinkingWaterModel DrinkingWater { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                SqlConnection connection = new(_configuration.GetConnectionString("DefaultConnectionString"));
+                connection.Open();
+                string query = $"INSERT INTO drinking_water(Date, Quantity) VALUES ('{DrinkingWater.Date}', {DrinkingWater.Quantity})";
+                SqlCommand command = new(query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                
+                return RedirectToPage("./Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToPage("./Error");
+            }
+
+        }
+    }
+}
