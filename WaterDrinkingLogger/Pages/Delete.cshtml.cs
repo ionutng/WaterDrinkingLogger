40826022528ada5b2950c3d4d0f8d9d2546bb772
ionutng <ionutngp@gmail.com>
2024-03-02 14:17:44 +0200

    Add the functionality to delete a record

---
+++ WaterDrinkingLogger/Pages/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using WaterDrinkingLogger.Models;
+
+namespace WaterDrinkingLogger.Pages;
+
+public class DeleteModel : PageModel
+{
+    private readonly IConfiguration _configuration;
+
+    [BindProperty]
+    public DrinkingWaterModel DrinkingWater { get; set; }
+
+    public DeleteModel(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult OnGet(int id)
+    {
+        DrinkingWater = GetById(id);
+
+        return Page();
+    }
+
+    private DrinkingWaterModel GetById(object id)
+    {
+        var drinkingWaterRecord = new DrinkingWaterModel();
+
+        SqlConnection connection = new(_configuration.GetConnectionString("DefaultConnectionString"));
+        connection.Open();
+
+        string query = $"SELECT * FROM drinking_water WHERE Id = {id}";
+        SqlCommand command = new(query, connection);
+        SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            drinkingWaterRecord.Id = reader.GetInt32(0);
+            drinkingWaterRecord.Date = DateOnly.FromDateTime(reader.GetDateTime(1));
+            drinkingWaterRecord.Quantity = reader.GetInt32(2);
+        }
+
+        return drinkingWaterRecord;
+    }
+
+    public IActionResult OnPost(int id)
+    {
+        SqlConnection connection = new(_configuration.GetConnectionString("DefaultConnectionString"));
+        connection.Open();
+        string query = $"DELETE FROM drinking_water WHERE Id = {id}";
+        SqlCommand command = new(query, connection);
+        command.ExecuteNonQuery();
+
+        return RedirectToPage("./Index");
+    }
+}
