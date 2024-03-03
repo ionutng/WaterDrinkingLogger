37cb3285df2c7676d4ea1c8baf713b27299a2d28
ionutng <ionutngp@gmail.com>
2024-03-03 12:01:09 +0200

    Add some validation

--- WaterDrinkingLogger/Models/DrinkingWaterModel.cs
+++ WaterDrinkingLogger/Models/DrinkingWaterModel.cs
@@ -1,8 +1,16 @@
-﻿namespace WaterDrinkingLogger.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WaterDrinkingLogger.Validation;
+
+namespace WaterDrinkingLogger.Models;
 
 public class DrinkingWaterModel
 {
     public int Id { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd-MMM-yy}", ApplyFormatInEditMode = true)]
+    [DateLessThanOrEqualToToday]
     public DateOnly Date { get; set; }
+
+    [Range(0, Int32.MaxValue, ErrorMessage = "Value for {0} must be positive.")]
     public int Quantity { get; set; }
 }
---
+++ WaterDrinkingLogger/Validation/DateLessThanOrEqualToToday.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace WaterDrinkingLogger.Validation;
+
+public class DateLessThanOrEqualToToday : ValidationAttribute
+{
+    public override string FormatErrorMessage(string name)
+    {
+        return "Date value should not be a future date";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var dateValue = value as DateOnly? ?? new DateOnly();
+
+        if (dateValue.CompareTo(DateOnly.FromDateTime(DateTime.Now.Date)) > 0)
+            return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+
+        return ValidationResult.Success;
+    }
+}
